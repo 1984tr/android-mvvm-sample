9c96bdc5b3d7a3b304c8163df0c271e76dec2e6e
1984tr <jollytris@gmail.com>
2020-01-06 23:36:53 +0900

    add list type

--- app/src/main/java/com/tr1984/mvvmsample/pages/list/ListViewModel.kt
+++ app/src/main/java/com/tr1984/mvvmsample/pages/list/ListViewModel.kt
@@ -1,7 +1,25 @@
 package com.tr1984.mvvmsample.pages.list
 
+import androidx.databinding.ObservableArrayList
 import com.tr1984.mvvmsample.base.BaseViewModel
 
 class ListViewModel : BaseViewModel() {
 
+    var items = ObservableArrayList<BaseViewModel>()
+
+    fun start() {
+        items.add(ListType01ViewModel().apply {
+            text.set("ListType01ViewModel")
+        })
+
+        items.add(ListType02ViewModel().apply {
+            text.set("ListType02ViewModel")
+            actionButton01 = {
+                toastSubject.onNext("BUTTON 01 !!!!")
+            }
+            actionButton02 = {
+                toastSubject.onNext(it.text.toString())
+            }
+        })
+    }
 }
