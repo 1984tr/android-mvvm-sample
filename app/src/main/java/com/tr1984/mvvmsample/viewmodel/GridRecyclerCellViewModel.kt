d65d7ae10b9538b46bed7205bb8c2427065fbbb6
1984tr <jollytris@gmail.com>
2020-01-10 23:09:15 +0900

    list in list 수정 중

--- app/src/main/java/com/tr1984/mvvmsample/viewmodel/GridRecyclerCellViewModel.kt
+++ app/src/main/java/com/tr1984/mvvmsample/viewmodel/GridRecyclerCellViewModel.kt
@@ -1,2 +1,10 @@
 package com.tr1984.mvvmsample.viewmodel
 
+import androidx.databinding.ObservableField
+import com.tr1984.mvvmsample.base.BaseViewModel
+
+class GridRecyclerCellViewModel : BaseViewModel() {
+
+    var text = ObservableField("")
+    var actionItemClick : (() -> Unit)? = null
+}
