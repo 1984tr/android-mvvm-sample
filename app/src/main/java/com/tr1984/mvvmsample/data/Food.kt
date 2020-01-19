2344d04697ce252d82b600e093eb81cb66e62567
1984tr <jollytris@gmail.com>
2020-01-19 23:16:14 +0900

    add detail

--- app/src/main/java/com/tr1984/mvvmsample/data/Food.kt
+++ app/src/main/java/com/tr1984/mvvmsample/data/Food.kt
@@ -4,4 +4,9 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "foods")
-data class Food(@PrimaryKey(autoGenerate = true) val id: Long, val imageUrl: String, val name: String = "", val likeCount: Int = 0)
+data class Food(@PrimaryKey(autoGenerate = true) val id: Long, val imageUrl: String, val name: String = "", val likeCount: Int = 0) {
+
+    companion object {
+        val dummy = Food(1, "https://cdn.pixabay.com/photo/2016/12/26/17/28/food-1932466__480.jpg", "", 0)
+    }
+}
--- app/src/main/java/com/tr1984/mvvmsample/pages/detail/DetailViewModel.kt
+++ app/src/main/java/com/tr1984/mvvmsample/pages/detail/DetailViewModel.kt
@@ -1,11 +1,16 @@
 package com.tr1984.mvvmsample.pages.detail
 
 import com.tr1984.mvvmsample.base.BaseViewModel
+import com.tr1984.mvvmsample.data.Food
 
 class DetailViewModel : BaseViewModel() {
 
+    var imageUrl: String = ""
+
     fun start(foodId: Long) {
         // local
-        //FoodsRepository.getInstance()
+        Food.dummy.let {
+            this@DetailViewModel.imageUrl = it.imageUrl
+        }
     }
 }
