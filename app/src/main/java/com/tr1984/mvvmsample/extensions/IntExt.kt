dbe61b443298ad711217af5796c80a19bfe403cb
1984tr <jollytris@gmail.com>
2020-01-20 23:07:03 +0900

    add detail

--- app/src/main/java/com/tr1984/mvvmsample/extensions/IntExt.kt
+++ app/src/main/java/com/tr1984/mvvmsample/extensions/IntExt.kt
@@ -1,8 +1,11 @@
 package com.tr1984.mvvmsample.extensions
 
 import android.content.res.Resources
+import java.text.NumberFormat
 
 fun Int.toPx(): Int {
     val metrics = Resources.getSystem().displayMetrics
     return (this * metrics.density + 0.5f).toInt()
 }
+
+fun Int.withComma() = NumberFormat.getInstance().format(this)
--- app/src/main/java/com/tr1984/mvvmsample/pages/detail/DetailViewModel.kt
+++ app/src/main/java/com/tr1984/mvvmsample/pages/detail/DetailViewModel.kt
@@ -1,16 +1,22 @@
 package com.tr1984.mvvmsample.pages.detail
 
+import androidx.databinding.ObservableField
 import com.tr1984.mvvmsample.base.BaseViewModel
 import com.tr1984.mvvmsample.data.Food
+import com.tr1984.mvvmsample.extensions.withComma
 
 class DetailViewModel : BaseViewModel() {
 
-    var imageUrl: String = ""
+    var fileName = ObservableField("")
+    var imageUrl = ObservableField("")
+    var likeCount = ObservableField("")
 
     fun start(foodId: Long) {
         // local
         Food.dummy.let {
-            this@DetailViewModel.imageUrl = it.imageUrl
+            this@DetailViewModel.fileName.set(it.imageUrl)
+            this@DetailViewModel.imageUrl.set(it.imageUrl)
+            this@DetailViewModel.likeCount.set(it.likeCount.withComma())
         }
     }
 }
