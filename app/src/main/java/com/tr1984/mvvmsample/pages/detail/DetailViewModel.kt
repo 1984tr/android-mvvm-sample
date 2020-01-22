622acb3d62f07e2bb1c7b0e04ff503e0a58b7ca6
1984tr <jollytris@gmail.com>
2020-01-22 23:04:17 +0900

    add detail

--- app/src/main/java/com/tr1984/mvvmsample/pages/detail/DetailViewModel.kt
+++ app/src/main/java/com/tr1984/mvvmsample/pages/detail/DetailViewModel.kt
@@ -3,20 +3,29 @@ package com.tr1984.mvvmsample.pages.detail
 import androidx.databinding.ObservableField
 import com.tr1984.mvvmsample.base.BaseViewModel
 import com.tr1984.mvvmsample.data.Food
-import com.tr1984.mvvmsample.extensions.withComma
 
 class DetailViewModel : BaseViewModel() {
 
     var fileName = ObservableField("")
     var imageUrl = ObservableField("")
-    var likeCount = ObservableField("")
+    var favorite = ObservableField(false)
+
+    private var isFavorite = false
+        set(value) {
+            field = value
+            favorite.set(field)
+        }
 
     fun start(foodId: Long) {
         // local
         Food.dummy.let {
             this@DetailViewModel.fileName.set(it.imageUrl)
             this@DetailViewModel.imageUrl.set(it.imageUrl)
-            this@DetailViewModel.likeCount.set(it.likeCount.withComma())
+            this@DetailViewModel.favorite.set(isFavorite)
         }
+    }
+
+    fun toggleFavorite() {
+        isFavorite = !isFavorite
     }
 }
